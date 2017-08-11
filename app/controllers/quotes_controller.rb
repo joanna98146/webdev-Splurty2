177a5cbd8d5cc075563ad82cf787ab4683eb366a
Joanna Tracy <joanna98146@yahoo.com>
2017-08-11 03:16:30 +0000

    added functionality to save the user input, limit entry length, and added if/then logic to display an error mesg.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,10 +5,16 @@ class QuotesController < ApplicationController
     def new
         @quote = Quote.new
     end
+    
      def create
-         Quote.create(quote_params)
+          @quote = Quote.create(quote_params)
+          
+  if @quote.invalid?
+    flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+  end
          redirect_to root_path
      end
+     
      private
 
    def quote_params
